81188a343c12e493dfed1c6a2b174794780a27d0
platipus25 <40722163+platipus25@users.noreply.github.com>
2021-01-15 22:30:56 -0800

    Cleanup things that should be in .gitignore

--- remappedSrc/com/platipus25/bluephantom/common/entity/SpectralPhantomModel.java
+++
@@ -1,7 +0,0 @@
-package com.platipus25.bluephantom.common.entity;
-
-import net.minecraft.client.render.entity.model.PigEntityModel;
-import net.minecraft.entity.Entity;
-
-public class SpectralPhantomModel<T extends Entity> extends PigEntityModel<T> {
-}
